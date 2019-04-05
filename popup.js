2cefb69dfa37fcd15b8d2047912d364fc34d7863
david <davidsramirez11@gmail.com>
2019-04-05 11:15:02 -0700

    style

--- popup.js
+++ popup.js
@@ -134,8 +134,8 @@ document.addEventListener('DOMContentLoaded', function() {
                    var div = document.createElement('div');
                     document.body.appendChild(div);
                     div.className = 'shia-do-it';
-                    div.style.cssText = ('position: fixed; bottom: 0px; z-index: 99999; left: 0px; height: 530px; width: 720px; pointer-events: none;');
-                    div.innerHTML = '<div class="shia-do-it--container" style="width: 720px; overflow:hidden; display:block; height: 535px;"><video width="100vw" height="100vh" style="margin-top: -5px; max-width: none; padding: 0;" name="media"><source type="video/webm"></video></div>';
+                    // div.style.cssText = ('position: fixed; bottom: 0px; z-index: 99999; left: 0px; height: 530px; width: 720px; pointer-events: none;');
+                    div.innerHTML = '<div class="shia-do-it--container" style="width: 100vw; overflow:hidden; display:block; height: 100hv;"><video width="100vw" height="100vh" style="margin-top: -5px; max-width: none; padding: 0;" name="media"><source type="video/webm"></video></div>';
                     var video = document.querySelector('.shia-do-it .shia-do-it--container video');
                     video.style.visibility = "hidden";
                     video.src = chrome.extension.getURL("/media/cena.mp4");
