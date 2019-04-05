7d0afdbbc3db58820a3f7b09bbf17038876bebfa
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2019-04-04 21:38:28 -0700

    move it mode and zoom mode

--- popup.js
+++ popup.js
@@ -4,33 +4,40 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
         // chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
         chrome.tabs.executeScript({
-            code: 'document.body.style.backgroundColor="black"'
+            code: `document.body.style.backgroundcolor='black'`
           });
       })
     }
-    function setCount(res) {
-        const div = document.createElement('div')
-        div.textContent = `${res.count} bears`
-        document.body.appendChild(div)
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('seniorMode').addEventListener('click', onclick, false)
+    function onclick () {
+      chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        // chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+        chrome.tabs.executeScript({
+            code: `document.body.style.zoom='30'`
+          });
+      })
     }
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    var nightMode = document.getElementById('seniorMode');
-    nightMode.addEventListener('click', function() {
-        chrome.tabs.getSelected(null, function(tab) {
-            alert('senior')
-        })
-    })
-})
 
 document.addEventListener('DOMContentLoaded', function() {
-    var nightMode = document.getElementById('moveitMode');
-    nightMode.addEventListener('click', function() {
-        chrome.tabs.getSelected(null, function(tab) {
-            alert('move')
-        })
-    })
+    document.getElementById('moveitMode').addEventListener('click', onclick, false)
+    function onclick () {
+      chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        // chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+        chrome.tabs.executeScript({
+           code: 
+           `var r = true;
+           
+           while (r) {
+               r = confirm("MOVE!");
+           }`
+          });
+      })
+    }
 })
 
 document.addEventListener('DOMContentLoaded', function() {
