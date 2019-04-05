89ef3b104104491c4e68a5c95ca3721189b07804
Anna Ingels <annaingels@Annas-MacBook-Pro.local>
2019-04-04 22:26:06 -0700

    nightmode

--- popup.js
+++ popup.js
@@ -4,7 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
         // chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
         chrome.tabs.executeScript({
-            code: `document.body.style.backgroundcolor='black'`
+            code: `const black = document.createElement('div')
+            black.style.backgroundColor='black';
+            black.style.height='100vh';
+            black.style.position='absolute';
+            black.style.top='0';
+            black.style.bottom='0';
+            black.style.left='0';
+            black.style.right='0';
+            black.textContent = 'hey';
+            document.body.appendChild(black)`
           });
       })
     }
