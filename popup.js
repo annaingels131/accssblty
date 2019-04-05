ef92784ebc95156ae47fcb07632b4f52b9c96db0
Maria Espinoza <phillyswirl002@mail.fresnostate.edu>
2019-04-04 20:42:59 -0700

    trying to add night mode

--- popup.js
+++ popup.js
@@ -2,9 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('nightMode').addEventListener('click', onclick, false)
     function onclick () {
       chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'hi')
+        // chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+        chrome.tabs.executeScript({
+            code: 'document.body.style.backgroundColor="black"'
+          });
       })
-    }   
+    }
+    function setCount(res) {
+        const div = document.createElement('div')
+        div.textContent = `${res.count} bears`
+        document.body.appendChild(div)
+    }
 })
 
 document.addEventListener('DOMContentLoaded', function() {
